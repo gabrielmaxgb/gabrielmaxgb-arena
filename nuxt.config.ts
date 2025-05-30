74b945ea4e4c7dfd80954c2c302cf935b9b1830b
gabrielmaxgb <gmaxgomes@gmail.com>
2025-05-30 12:48:43 -0300

    ++

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
+	compatibilityDate: "2025-05-15",
+	devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxt/ui'
-  ]
-})
+	ssr: true,
+	nitro: {
+		preset: "github_pages",
+	},
+	app: {
+		baseURL: "/gabrielmaxgb/",
+	},
+
+	modules: [
+		"@nuxt/content",
+		"@nuxt/eslint",
+		"@nuxt/fonts",
+		"@nuxt/icon",
+		"@nuxt/image",
+		"@nuxt/scripts",
+		"@nuxt/test-utils",
+		"@nuxt/ui",
+	],
+});
