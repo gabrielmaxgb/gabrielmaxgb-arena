9d398c1a8e55bcc01d910de3739cbd288013bdb1
gabrielmaxgb <gabrielmaxgb@users.noreply.github.com>
2025-06-01 19:54:21 +0000

    deploy: f5024ec70aea2463226473f8fb4036ad8fa6fe4c

---
+++ _nuxt/C07HoqsY.js
@@ -0,0 +1,1 @@
+import{_ as o,c as r,o as t,r as c}from"./Ctx3jLAH.js";const s={};function n(e,l){return t(),r("blockquote",null,[c(e.$slots,"default")])}const _=o(s,[["render",n]]);export{_ as default};
