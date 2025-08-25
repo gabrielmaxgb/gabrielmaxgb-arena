90b7d9df70352666489a93947dda9c7e5122fd35
gabrielmaxgb <gmaxgomes@gmail.com>
2025-08-25 00:50:36 -0300

    adds new article

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
-const articles = [
+const articles: {
+	title: string;
+	url: string;
+	readTimeMinutes: number;
+	publishedAt: string;
+	language: string;
+}[] = [
 	{
 		title: "Quero estudar programação. Pra quê Linux?",
 		url: "https://medium.com/@gabriellmax/quero-estudar-programa%C3%A7%C3%A3o-pra-qu%C3%AA-linux-bb5312b02f1c",
@@ -22,28 +28,13 @@ const articles = [
 		publishedAt: "may 3, 2025",
 		language: "en-US",
 	},
-	// {
-	// 	title: "Quero estudar programação. Pra quê Linux?",
-	// 	url: "https://medium.com/@gabriellmax/quero-estudar-programa%C3%A7%C3%A3o-pra-qu%C3%AA-linux-bb5312b02f1c",
-	// 	readTimeMinutes: 7,
-	// 	publishedAt: "oct 9, 2020",
-	// 	language: "pt-BR",
-	// },
-	// {
-	// 	title:
-	// 		"A Montanha Sem Placas: Quando o Front-End Ignora as Regras de Negócio",
-	// 	url: "https://medium.com/@gabriellmax/a-montanha-sem-placas-quando-o-front-end-ignora-as-regras-de-neg%C3%B3cio-2f0b1c6d7a4e",
-	// 	readTimeMinutes: 6,
-	// 	publishedAt: "may 3, 2025",
-	// 	language: "pt-BR",
-	// },
-	// {
-	// 	title: "The Unmarked Mountain: When Front-End Ignores Business Rules",
-	// 	url: "https://gabrielmaxgb.medium.com/the-unmarked-mountain-when-front-end-ignores-business-rules-bd5f1f55bafe",
-	// 	readTimeMinutes: 6,
-	// 	publishedAt: "may 3, 2025",
-	// 	language: "en-US",
-	// },
+	{
+		title: "Princípios de Design de Software aplicados ao Front-end",
+		url: "https://medium.com/@gabrielmaxgb/princ%C3%ADpios-de-design-de-software-aplicados-ao-front-end-7beb86594ca2",
+		readTimeMinutes: 6,
+		publishedAt: "aug 25, 2025",
+		language: "pt-BR",
+	},
 ];
 </script>
 
